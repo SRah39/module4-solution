14387e3529f9bddb06834f4ddc85f8a7eb77fd54
SRah39 <sonir1993@gmail.com>
2017-05-21 18:33:26 -0400

    minor fix

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
-var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+(function() {
+	var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 
-for (var name in names) {
+	for (var name in names) {
 
-  var firstLetter = ((names[name]).charAt(0)).toLowerCase();
+	  var firstLetter = ((names[name]).charAt(0)).toLowerCase();
 
-  if (firstLetter == 'j') {
-    window.byeSpeaker.speak(names[name]);
-  } else {
-    window.helloSpeaker.speak(names[name]);
-  }
-  
-}
+	  if (firstLetter == 'j') {
+	    window.byeSpeaker.speak(names[name]);
+	  } else {
+	    window.helloSpeaker.speak(names[name]);
+	  }
+	  
+	}
+})();
